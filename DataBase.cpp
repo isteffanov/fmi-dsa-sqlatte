87c8b1b634d27bb7e8d5c7f934f89eaf4b1f1f81
isteffanov <isteffanov@gmail.com>
2021-02-03 15:46:31 +0200

    merged filemanager with database

--- DataBase.cpp
+++ DataBase.cpp
@@ -1,144 +1,305 @@
 #include "DataBase.hpp"
 
-DataBase::DataBase() {
-	database = filemanager.get_tables();
+const std::string DataBase::DB_RELATIVE_PATH = "database/";
+const std::string DataBase::DB_FILE = DB_RELATIVE_PATH + "db.bin";
+
+DataBase::DataBase()
+{
+	out.open(DB_FILE.c_str(), std::ios::binary | std::ios::app);
+	in.open(DB_FILE.c_str(), std::ios::binary);
 }
 
 DataBase::~DataBase()
 {
-}
-
-void DataBase::create(const std::string& name, const table_row& names, const table_row& types)
-{
-	Table* newTable = new Table(name, names, types);
-	database.push_back(newTable);
-	filemanager.create_table(newTable);
-}
-
-void DataBase::drop(const std::string& name)
-{
-	list_tableptr_it table_it = find(name);
-	if (table_it == database.end()) {
-		std::cerr << "No such table!" << std::endl;
-		return;
-	}
-
-	delete (*table_it);
-	database.erase(table_it);
-	filemanager.remove_table(name);
-	std::cout << "Table dropped!" << std::endl;
+	out.close();
+	in.close();
 }
 
 void DataBase::info(const std::string& name)
 {
-	list_tableptr_it table_it = find(name);
-
-	if (table_it != database.end()) (*table_it)->info();
+	Table* table = initTable(name);
+	
+	if (table) table->info(size(name));
 	else std::cerr << "No such table!" << std::endl;
 }
 
-void DataBase::list() const
+void DataBase::list()
 {
+	std::list<Table*> database = get_tables();
+
 	if (database.size() == 0) std::cout << "There are no tables in the database!" << std::endl;
 	else if (database.size() == 1) std::cout << "There is 1 table in the database:" << std::endl;
 	else std::cout << "There are " << database.size() << " tables in the database:" << std::endl;
-
+	
 	for (const Table* const table : database)
 		std::cout << "\t" << table->name() << std::endl;
 }
 
-void DataBase::insert(const std::string& name, const table_row& record)
+bool DataBase::create_table(const std::string& name, const table_row& names, const table_row& types)
 {
-	Table* table = find_ptr(name);
-	if (!table) {
+	const Table* table = new Table(name, Schema(names, types));
+
+	if(!write_string(out, table->name())) return false;
+
+	if (!(out << table->getSchema())) return false;
+
+	save();
+
+	std::string fileName = DB_RELATIVE_PATH + table->name() + ".bin";
+	std::ofstream file = getStreamOUT(fileName); //create the table file
+	file.close();
+
+	return true;
+}
+
+void DataBase::remove_table(const std::string& table)
+{
+	std::ofstream temp("temp.bin", std::ios::binary);
+
+	std::string toRemove = table + ".bin";
+	if (std::remove(toRemove.c_str())) {						//delete old file
+		std::cerr << "Remove failed!" << std::endl;
+		std::remove("temp.bin");
+		return;
+	}
+
+	size_t len;
+	char* ptr;
+	while (in.read((char*)&len, sizeof(size_t))) {								//move other content to new file
+		ptr = new char[len + 1];
+
+		in.read(ptr, len);
+		ptr[len] = '\0';
+
+		if (!table.compare(ptr)) {
+			delete[] ptr;
+			continue;
+		}
+
+		temp.write((const char*)&len, sizeof(size_t));
+		temp.write(ptr, len);
+
+		delete[] ptr;
+	}
+
+	std::rename("temp.bin", toRemove.c_str());
+}
+
+void DataBase::insert_records(const std::string& table_name, const std::list<Record>& recs)
+{
+	std::ofstream table = getStreamOUT(table_name);
+	if (!table.is_open()) {
 		std::cerr << "No such table!" << std::endl;
 		return;
 	}
 
-	const Record rec = table->insert(record);
-	filemanager.insert_record(name, rec);
+	for(const Record& rec: recs)
+		table << rec;
+
+	table.close();
 }
 
-void DataBase::remove(const std::string& name, const std::string& query)
+void DataBase::remove_records(const std::string& table, std::list<Record>& found)
 {
-	Table* table = find_ptr(name);
+	std::ifstream from = getStreamIN(table);
+	std::ofstream to = getStreamOUT("temp");
 
-	if (table) {
-		std::list<Record> found = table->remove(query);
-		for (const Record& rec : found)
-			filemanager.remove_record(name, rec);
-	}
-	else std::cerr << "No such table!" << std::endl;
-}
-
-void DataBase::selectAll(const std::string& name, const std::string& query) const
-{
-	Table* table = find_ptr(name);
-
-	if (table) table->selectAll(query);
-	else std::cerr << "No such table!" << std::endl;
-}
-
-void DataBase::selectSome(const std::string& name, const table_row& cols, const std::string& query) const
-{
-	Table* table = find_ptr(name);
-
-	if (table) table->selectSome(query, cols);
-	else std::cerr << "No such table!" << std::endl;
-}
-
-size_t DataBase::size_of(const std::string& table)
-{
-	Table* searched = find_ptr(table);
-	if (!searched) return 0;
-
-	return searched->size();
-}
-
-typename DataBase::list_tableptr_it DataBase::find(const std::string& name) 
-{
-	for (list_tableptr_it it = database.begin(); it != database.end(); ++it)
-		if ((*it)->name() == name) return it;
-
-	return database.end();
-}
-
-Table* DataBase::find_ptr(const std::string& name) const
-{
-	for (Table* table : database)
-		if (table->name() == name) return table;
-
-	return nullptr;
-}
-
-std::ifstream& operator>>(std::ifstream& in, DataBase*& db)
-{
-	size_t size;
-	in.read((char*)&size, sizeof(size_t));
-
-	std::string cur;
-	for (int i = 0; i < size; ++i) {
-		read_string(in, cur);
-
-		std::ifstream file(cur);
-		if (file.is_open()) {
-			Table* ptr;
-			file >> ptr;
-			db->database.push_back(ptr);
-
-			file.close();
+	Record cur;
+	bool same = false;
+	while (from >> cur) {								//move other content to new file
+		for (const Record rec : found) {
+			if (rec == cur) {
+				same = true;
+				break;
+			}
 		}
+
+		if(!same) to << cur;
+		same = false;
 	}
-	return in;
+
+	from.close();
+	to.close();
+
+	std::string del = DB_RELATIVE_PATH + table + ".bin";
+	std::string tmp = DB_RELATIVE_PATH + "temp.bin";
+
+	int res = std::remove(del.c_str());
+	if( res != 0 ){		//delete old file
+		std::cerr << "Remove failed!" << strerror(errno) << '\n';
+		std::remove(tmp.c_str());
+		return;
+	}
+
+	std::rename(tmp.c_str(), del.c_str());
 }
 
-std::ofstream& operator<<(std::ofstream& out, const DataBase*& db)
+void DataBase::removeQuery(const std::string& tableName, const std::string& query)
 {
-	size_t size = db->database.size();
-	out.write((const char*)&size, sizeof(size_t));
+	Table* table = initTable(tableName);
+	if (!table) return;
+	std::ifstream tableFile = getStreamIN(tableName);
 
-	for (const Table* table : db->database)
-		write_string(out, table->name());
+	BinaryQueryTree bq_tree(table);
+	bq_tree.init(query);
 
-	return out;
+	while(table->getContents(tableFile, MAX_ROWS_RETURN)){
+		
+		tableFile.close();
+		std::list<Record> found = bq_tree.search();
+
+		remove_records(tableName, found);
+
+		table->clear();
+		tableFile = getStreamIN(tableName);
+	}
+
+	tableFile.close();
+	table->reset();
+}
+
+void DataBase::selectAll(const std::string& tableName, const std::string& query)
+{
+	Table* table = initTable(tableName);
+	if (!table) return;
+	std::ifstream tableFile = getStreamIN(tableName);
+
+	BinaryQueryTree bq_tree(table);
+	bq_tree.init(query);
+
+	while (table->getContents(tableFile, MAX_ROWS_RETURN)) {
+
+		tableFile.close();
+		std::list<Record> found = bq_tree.search();
+
+		std::cout << tableName << std::endl;
+		printSelected(found);
+
+		table->clear();
+		tableFile = getStreamIN(tableName);
+	}
+
+	tableFile.close();
+	table->reset();
+
+	//print elements in every iteration
+}
+
+void DataBase::selectSome(const std::string& tableName, const table_row& cols, const std::string& query) 
+{
+	Table* table = initTable(tableName);
+	std::vector<bool> selectedColumns = table->getSchema().columns(cols);
+
+	if (!table) return;
+	std::ifstream tableFile = getStreamIN(tableName);
+
+	BinaryQueryTree bq_tree(table);
+	bq_tree.init(query);
+
+	while (table->getContents(tableFile, MAX_ROWS_RETURN)) {
+
+		tableFile.close();
+		std::list<Record> found = bq_tree.search();
+
+		std::cout << tableName << std::endl;
+
+		printSelected(found, selectedColumns);
+
+		table->clear();
+		tableFile = getStreamIN(tableName);
+	}
+
+	tableFile.close();
+	table->reset();
+}
+
+std::list<Table*> DataBase::get_tables()
+{
+	std::list<Table*> rtrn;
+	std::string tableName;
+	
+	std::string name;
+	Schema schema;
+
+	while (true) {
+		read_string(in, name);
+		in >> schema;
+
+		if (!in) break;
+		rtrn.push_back(new Table(name, schema));
+	}
+
+	return rtrn;
+}
+
+Table* DataBase::getTable(const std::string& tableName) const
+{
+	Table* table = new Table(tableName);
+	std::ifstream tableFile = getStreamIN(tableName);
+
+	if (tableFile.is_open()) {
+		tableFile >> table;
+		tableFile.close();
+	}
+
+	return table;
+}
+
+Table* DataBase::initTable(const std::string& tableName) 
+{
+	std::string name;
+	Schema schema;
+	do {
+		read_string(in, name);
+		in >> schema;
+		if (!in) break;
+	} while (name != tableName);
+
+	if (name != "") return new Table(name, schema);
+	else return nullptr;
+}
+
+void DataBase::save()
+{
+	in.close();
+	out.close();
+	in.open(DB_FILE.c_str(), std::ios::binary);
+	out.open(DB_FILE.c_str(), std::ios::binary | std::ios::app);
+}
+
+uint64_t DataBase::size(const std::string& name) const
+{
+	std::fstream stream((name + ".bin").c_str());
+
+	stream.seekg(0L, SEEK_END);
+	uint64_t ans = stream.tellg();
+	stream.close();
+
+	return ans;
+}
+
+void DataBase::printSelected(const std::list<Record>& found) const
+{
+	for (const Record& rec : found)
+		rec.print();
+}
+
+void DataBase::printSelected(const std::list<Record>& found, const std::vector<bool>& cols)
+{
+	for (const Record& rec : found)
+		rec.print(cols);
+}
+
+std::ifstream DataBase::getStreamIN(const std::string& table) const
+{
+	std::string path = DB_RELATIVE_PATH + table + ".bin";
+	std::ifstream file(path.c_str(), std::ios::binary);
+	return file;
+}
+
+std::ofstream DataBase::getStreamOUT(const std::string& table) const
+{
+	std::string path = DB_RELATIVE_PATH + table + ".bin";
+	std::ofstream file(path.c_str(), std::ios::binary | std::ios::app);
+	return file;
 }
--- DataBase.hpp
+++ DataBase.hpp
@@ -1,41 +1,51 @@
 #pragma once
-#include <iostream>
-#include <list>
+#include <fstream>
 #include <string>
 
-#include "FileManager.hpp"
-#include "Table.hpp"
+#include "BinaryQueryTree.hpp"
 #include "read_write_helpers.hpp"
+#include "Table.hpp"
 
 class DataBase
 {
 #include "typedefs.ipp"
+	static const std::string DB_RELATIVE_PATH;
+	static const std::string DB_FILE;
 
-	std::list<Table*> database;
-	FileManager filemanager;
+	std::ifstream in;
+	std::ofstream out;
 
+	static const int MAX_ROWS_RETURN = 500;
 public:
 	DataBase();
-	DataBase(const DataBase& other) = delete;
-	DataBase& operator=(const DataBase& other) = delete;
+	DataBase(const DataBase&) = delete;
+	const DataBase& operator=(const DataBase&) = delete;
 	~DataBase();
 
-	void				create(const std::string& name, const table_row& names, const table_row& types);
-	void				drop(const std::string& name);
-	void				info(const std::string& name);
-	void				list() const;
+	void info(const std::string& name);
+	void list();
 
-	void				insert(const std::string& name, const table_row& record);
-	void				remove(const std::string& name, const std::string& query);
-	void				selectAll(const std::string& name, const std::string& query) const;
-	void				selectSome(const std::string& name, const table_row& cols, const std::string& query) const;
-	size_t				size_of(const std::string& table);
+	bool create_table(const std::string& name, const table_row& names, const table_row& types);
+	void remove_table(const std::string& table);
+	void insert_records(const std::string& table, const std::list<Record>& rec);
+	void remove_records(const std::string& table, std::list<Record>& found);
 
-	friend std::ifstream& operator>>(std::ifstream& in, DataBase*& table);
-	friend std::ofstream& operator<<(std::ofstream& out, const DataBase*& table);
+	void removeQuery(const std::string& table, const std::string& query);
+
+	void		selectAll(const std::string& name, const std::string& query);
+	void	    selectSome(const std::string& name, const table_row& cols, const std::string& query);
+
+	std::list<Table*> get_tables();
+
+	Table* getTable(const std::string& tableName) const;
+	Table* initTable(const std::string& tableName); //get empty table with its schema
 
 private:
-	list_tableptr_it	find(const std::string& name);
-	Table*				find_ptr(const std::string& name) const;
+	void save();
+	uint64_t size(const std::string& name) const;
+	void printSelected(const std::list<Record>& found) const;
+	void printSelected(const std::list<Record>& found, const std::vector<bool>& cols);
+	std::ifstream getStreamIN(const std::string& table) const;
+	std::ofstream getStreamOUT(const std::string& table) const;
 };
 
